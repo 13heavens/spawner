d0338a337b3a86db52c67ec276f3530c75ffda49
martin <exze.real@gmail.com>
2025-09-01 22:11:36 +0200

    changing NBT logic

--- spawner/spawner.go
+++ spawner/spawner.go
@@ -49,18 +49,57 @@ func (s Spawner) Activate(pos cube.Pos, clickedFace cube.Face, tx *world.Tx, u i
 
 // DecodeNBT ...
 func (s Spawner) DecodeNBT(data map[string]any) any {
-	s.Delay = int(castOr[int16](data["Delay"], 0))
-	s.Movable = castOr[byte](data["isMovable"], 0) == 1
-	s.RequiredPlayerRange = int(castOr[int16](data["RequiredPlayerRange"], 0))
-	s.MaxNearbyEntities = int(castOr[int16](data["MaxNearbyEntities"], 0))
-	s.MaxSpawnDelay = int(castOr[int16](data["MaxSpawnDelay"], 0))
-	s.MinSpawnDelay = int(castOr[int16](data["MinSpawnDelay"], 0))
-	s.SpawnCount = int(castOr[int16](data["SpawnCount"], 0))
-	s.SpawnRange = int(castOr[int16](data["SpawnRange"], 0))
+	// 1) Start with vanilla-ish defaults:
+	s.Delay = 20
+	s.Movable = true
+	s.RequiredPlayerRange = 16
+	s.MaxNearbyEntities = 6
+	s.MaxSpawnDelay = 800
+	s.MinSpawnDelay = 200
+	s.SpawnCount = 4
+	s.SpawnRange = 4
 
-	if id := castOr[string](data["EntityIdentifier"], ""); id != "" {
-		s.EntityType = entities[id]
+	// 2) Overlay NBT if present:
+	if v, ok := nbtInt(data, "Delay"); ok {
+		// allow 0 to be kept
+		if v >= 0 {
+			s.Delay = v
+		}
 	}
+	if v, ok := nbtByte(data, "isMovable"); ok {
+		s.Movable = v == 1
+	}
+	if v, ok := nbtInt(data, "RequiredPlayerRange"); ok && v > 0 {
+		s.RequiredPlayerRange = v
+	}
+	if v, ok := nbtInt(data, "MaxNearbyEntities"); ok && v > 0 {
+		s.MaxNearbyEntities = v
+	}
+	if v, ok := nbtInt(data, "MaxSpawnDelay"); ok && v > 0 {
+		s.MaxSpawnDelay = v
+	}
+	if v, ok := nbtInt(data, "MinSpawnDelay"); ok && v > 0 {
+		s.MinSpawnDelay = v
+	}
+	if v, ok := nbtInt(data, "SpawnCount"); ok && v > 0 {
+		s.SpawnCount = v
+	}
+	if v, ok := nbtInt(data, "SpawnRange"); ok && v > 0 {
+		s.SpawnRange = v
+	}
+
+	// Entity ID
+	if id, ok := data["EntityIdentifier"].(string); ok && id != "" {
+		if et, ok2 := entities[id]; ok2 {
+			s.EntityType = et
+		}
+	}
+
+	// 3) Sanity: make sure delays are consistent.
+	if s.MaxSpawnDelay < s.MinSpawnDelay {
+		s.MaxSpawnDelay = s.MinSpawnDelay
+	}
+
 	return s
 }
 
@@ -76,28 +115,87 @@ func castOr[T any](v any, or T) T {
 	}
 }
 
+func nbtInt(m map[string]any, key string) (int, bool) {
+	v, ok := m[key]
+	if !ok || v == nil {
+		return 0, false
+	}
+	switch x := v.(type) {
+	case int:
+		return x, true
+	case int8:
+		return int(x), true
+	case int16:
+		return int(x), true
+	case int32:
+		return int(x), true
+	case int64:
+		return int(x), true
+	case uint8:
+		return int(x), true
+	case uint16:
+		return int(x), true
+	case uint32:
+		return int(x), true
+	case uint64:
+		return int(x), true
+	case float32:
+		return int(x), true
+	case float64:
+		return int(x), true
+	default:
+		return 0, false
+	}
+}
+
+func nbtByte(m map[string]any, key string) (byte, bool) {
+	v, ok := m[key]
+	if !ok || v == nil {
+		return 0, false
+	}
+	switch x := v.(type) {
+	case byte:
+		return x, true
+	case int8:
+		return byte(x), true
+	case int16:
+		return byte(x), true
+	case int32:
+		return byte(x), true
+	case int:
+		return byte(x), true
+	default:
+		return 0, false
+	}
+}
+
 // EncodeNBT ...
 func (s Spawner) EncodeNBT() map[string]any {
 	var entityID string
 	if s.EntityType != nil {
 		entityID = s.EntityType.EncodeEntity()
 	}
+
 	return map[string]any{
-		"Delay":               int16(s.Delay),
-		"DisplayEntityHeight": float32(1),
-		"DisplayEntityWidth":  float32(1),
-		"EntityIdentifier":    entityID,
-		"MaxNearbyEntities":   int16(s.MaxNearbyEntities),
-		"MaxSpawnDelay":       int16(s.MaxSpawnDelay),
-		"MinSpawnDelay":       int16(s.MinSpawnDelay),
-		"RequiredPlayerRange": int16(s.RequiredPlayerRange),
+		"id":               "MobSpawner",
+		"EntityIdentifier": entityID,
+
+		"Delay":         int16(s.Delay),
+		"MinSpawnDelay": int16(s.MinSpawnDelay),
+		"MaxSpawnDelay": int16(s.MaxSpawnDelay),
+
 		"SpawnCount":          int16(s.SpawnCount),
 		"SpawnRange":          int16(s.SpawnRange),
-		"id":                  "MobSpawner",
+		"MaxNearbyEntities":   int16(s.MaxNearbyEntities),
+		"RequiredPlayerRange": int16(s.RequiredPlayerRange),
+
+		"DisplayEntityHeight": float32(1),
+		"DisplayEntityWidth":  float32(1),
 		"isMovable":           boolToByte(s.Movable),
-		"x":                   int32(s.pos.X()),
-		"y":                   int32(s.pos.Y()),
-		"z":                   int32(s.pos.Z()),
+
+		"x": int32(s.pos.X()),
+		"y": int32(s.pos.Y()),
+		"z": int32(s.pos.Z()),
 	}
 }
 
@@ -105,6 +203,7 @@ func boolToByte(b bool) byte {
 	if b {
 		return 1
 	}
+
 	return 0
 }
 
@@ -113,6 +212,7 @@ func (s Spawner) Tick(_ int64, pos cube.Pos, tx *world.Tx) {
 	if s.EntityType == nil {
 		return
 	}
+
 	s.pos = pos
 	s.Delay--
 
@@ -121,34 +221,36 @@ func (s Spawner) Tick(_ int64, pos cube.Pos, tx *world.Tx) {
 		return
 	}
 
-	minRange := s.RequiredPlayerRange
-	p1, p2 := s.pos.Add(cube.Pos{-minRange, -minRange, -minRange}), s.pos.Add(cube.Pos{minRange, minRange, minRange})
-	x0, y0, z0, x1, y1, z1 := float64(p1.X()), float64(p1.Y()), float64(p1.Z()), float64(p2.X()), float64(p2.Y()), float64(p2.Z())
+	p1 := s.pos.Add(cube.Pos{-s.RequiredPlayerRange, -s.RequiredPlayerRange, -s.RequiredPlayerRange})
+	p2 := s.pos.Add(cube.Pos{s.RequiredPlayerRange, s.RequiredPlayerRange, s.RequiredPlayerRange})
+	box := cube.Box(float64(p1.X()), float64(p1.Y()), float64(p1.Z()), float64(p2.X()), float64(p2.Y()), float64(p2.Z()))
 
-	var playerCount int
-	for e := range tx.EntitiesWithin(cube.Box(x0, y0, z0, x1, y1, z1)) {
+	playerNearby := false
+	for e := range tx.EntitiesWithin(box) {
 		if e.H().Type() == player.Type {
-			playerCount++
+			playerNearby = true
+			break
 		}
 	}
-	if playerCount <= 0 {
+
+	if !playerNearby {
 		return
 	}
 
-	var nearbyEntityCount int
-	for e := range tx.EntitiesWithin(cube.Box(x0, y0, z0, x1, y1, z1)) {
+	// Count nearby entities of same type
+	nearbyCount := 0
+	for e := range tx.EntitiesWithin(box) {
 		if e.H().Type() == s.EntityType {
-			nearbyEntityCount++
+			nearbyCount++
+			if nearbyCount >= s.MaxNearbyEntities {
+				return // too many already
+			}
 		}
 	}
-	if nearbyEntityCount <= 0 {
-		return
-	}
 
-	s.SpawnCount = rand.Intn(4)
+	spawnCount := rand.Intn(s.SpawnCount)
 	blockPos := pos.Vec3()
-
-	for i := 0; i < s.SpawnCount; i++ {
+	for i := 0; i < spawnCount; i++ {
 		var spawnPos mgl64.Vec3
 
 		if rand.Float64() > 0.5 {
@@ -161,6 +263,7 @@ func (s Spawner) Tick(_ int64, pos cube.Pos, tx *world.Tx) {
 		if !ok {
 			return
 		}
+
 		tx.AddEntity(newEnt(cube.PosFromVec3(spawnPos), tx))
 	}
 
